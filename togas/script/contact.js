ce32df724ebaadf5432131335e100af4a4e24225
galiciaabraham <galicia.abraham@hotmail.com>
2023-11-23 12:51:00 -0600

    added functionality to the contact button

--- togas/script/contact.js
+++ togas/script/contact.js
@@ -1,3 +1,9 @@
 import { hamburgerMenu,getCurrentYear } from "./utils.mjs";
+
 getCurrentYear();
 hamburgerMenu();
+
+const contactButton = document.querySelector(".contact-button");
+contactButton.addEventListener("click", ()=>{
+    window.location.href = "./form-submission.html";
+})
