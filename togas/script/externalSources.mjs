d51516001423db70c3eb42ff625c8d38a30ae20d
galiciaabraham <galicia.abraham@hotmail.com>
2023-12-12 13:56:36 -0600

    changed to a Web API

--- togas/script/externalSources.mjs
+++ togas/script/externalSources.mjs
@@ -1,3 +1,9 @@
+export async function getDatafromAPI(url){
+  let response = await fetch(url);
+  let result = convertToJson(response);
+  return result;
+}
+
 export async function getDataFromJSON(jsonFile){
     let response = await fetch(jsonFile);
     let result = convertToJson(response);
--- togas/script/portfolio.mjs
+++ togas/script/portfolio.mjs
@@ -1,2 +1,11 @@
+import { getDatafromAPI, getDataFromJSON } from "./externalSources.mjs";
 
+const apiJSONFile = "https://galiciaabraham.github.io/wdd330-togas/togas/json/apikey.json";
+const apiKey = await getDataFromJSON(apiJSONFile);
+const url1 = `https://pixabay.com/api/?key=${apiKey}&q="graduation"&orientation="horizontal"`;
+
+export async function getPhotos(url = url1){
+    let photos = getDatafromAPI(url);
+    console.log(photos);
+}
 
